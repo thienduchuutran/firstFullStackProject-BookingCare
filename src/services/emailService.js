78bdab4769c89c1c592d2411bbc17b1bb895e4d9
Duc <thienductranhuu2784@gmail.com>
2024-08-25 15:11:06 -0400

    making email subject and body content more flexible in viet and eng

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 import nodemailer from 'nodemailer'
 
 let sendSimpleEmail = async(dataSend) => {
+    let result =  getBodyHTMLEmail(dataSend)
     let transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
         port: 465,
@@ -15,16 +16,16 @@ let sendSimpleEmail = async(dataSend) => {
       let info = await transporter.sendMail({
         from: '"Duc Tran 👻" <thienductranhuu2784@gmail.com>', // sender address
         to: dataSend.receiverEmail, // list of receivers
-        subject: "Thông tin đặt lịch khám bệnh ✔", // Subject line
+        subject: result.subject, // Subject line
         text: "Hello world?", // plain text body
-        html: getBodyHTMLEmail(dataSend), 
+        html: result.html, 
       });
 }
 
 let getBodyHTMLEmail = (dataSend) => {
-  let result = ''
+  let result = {}
   if(dataSend.language === 'vi'){
-    result = `
+    result.html = `
         <h3>Xin chào ${dataSend.patientName}</h3>
         <p>Bạn nhận được email này vì đã đặt lịch khám bệnh với Duc Tran Booking Care</p>
         <p>Thông tin dặt lịch khám bệnh:</p>
@@ -39,10 +40,14 @@ let getBodyHTMLEmail = (dataSend) => {
         </div>
 
         <div>Chân thành cảm ơn</div>
-        `
+        `,
+      result.subject = `
+      Thông tin đặt lịch khám bệnh ✔
+      `
   }
   if(dataSend.language === 'en'){
-    result = `<h3>Hello ${dataSend.patientName}</h3> <p>You received this email because you have scheduled a medical appointment with Duc Tran Booking Care</p> <p>Appointment details:</p> <div><b>Time: ${dataSend.time}</b></div> <div><b>Doctor: ${dataSend.doctorName}</b></div> <p>If the information above is correct, please click the link below to confirm and complete the appointment process.</p> <div> <a href=${dataSend.redirectLink} target="_blank">Click here</a> </div> <div>Thank you very much</div>`
+    result.html = `<h3>Hello ${dataSend.patientName}</h3> <p>You received this email because you have scheduled a medical appointment with Duc Tran Booking Care</p> <p>Appointment details:</p> <div><b>Time: ${dataSend.time}</b></div> <div><b>Doctor: ${dataSend.doctorName}</b></div> <p>If the information above is correct, please click the link below to confirm and complete the appointment process.</p> <div> <a href=${dataSend.redirectLink} target="_blank">Click here</a> </div> <div>Thank you very much</div>`,
+    result.subject = `Appointment scheduling information ✔`
   }
 
   return result
