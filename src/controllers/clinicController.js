a929144a8d3ea8bf286dfa5394271ef6f05c0a4f
Duc <thienductranhuu2784@gmail.com>
2024-08-29 20:10:24 -0400

    API to get all clinics data

--- src/controllers/clinicController.js
+++ src/controllers/clinicController.js
@@ -2,7 +2,7 @@ import clinicService from '../services/clinicService'
 
 let createClinic = async(req, res) => {
     try{
-        let info = await specialtyService.createClinic(req.body)   //we are just sending all info to server, thats why req.body not req.query
+        let info = await clinicService.createClinic(req.body)   //we are just sending all info to server, thats why req.body not req.query
         return res.status(200).json(                                                            
             info
         )
@@ -15,6 +15,38 @@ let createClinic = async(req, res) => {
     }
 }
 
+let getAllClinic = async (req, res) => {
+    try{
+        let info = await clinicService.getAllClinic()   //we are just sending all info to server, thats why req.body not req.query
+        return res.status(200).json(                                                            
+            info
+        )
+    }catch(e){
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })  
+    }
+}
+
+let getDetailClinicById = async (req, res) => {
+    try{
+        let info = await clinicService.getDetailClinicById(req.query.id)   //we are just sending all info to server, thats why req.body not req.query
+        return res.status(200).json(                                                            
+            info
+        )
+    }catch(e){
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })  
+    }    
+}
+
 module.exports = {
-    createClinic: createClinic
+    createClinic: createClinic,
+    getAllClinic: getAllClinic,
+    getDetailClinicById: getDetailClinicById
 }
--- src/services/clinicService.js
+++ src/services/clinicService.js
@@ -33,6 +33,36 @@ let createClinic = (data) => {
     })
 }
 
+let getAllClinic = () => {
+    return new Promise(async(resolve, reject) => {
+        try{
+            let data = await db.Clinic.findAll()     //in reality we gotta limit to optimize the query and API
+            
+            //this is to convert (encode) image from blob to binary
+            if(data && data.length > 0){
+                data.map(item => {
+                    item.image = Buffer.from(item.image, 'base64').toString('binary')    //HTML can only understand string type aka base64, but db is saving as binary type so this step is to convert back to binary
+                    return item
+                })
+            }
+        }catch(e){
+            reject(e)
+        }
+    })
+}
+
+let getDetailClinicById = () => {
+    return new Promise(async(resolve, reject) => {
+        try{
+
+        }catch(e){
+            reject(e)
+        }
+    })
+}
+
 module.exports = {
-    createClinic: createClinic
+    createClinic: createClinic,
+    getAllClinic: getAllClinic,
+    getDetailClinicById: getDetailClinicById
 }
